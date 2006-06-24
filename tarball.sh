d931d2d34303189df0ccd696e97e3b46c3e76b1b
Diego 'Flameeyes' Pettenò <flameeyes@gentoo.org>
2006-06-25 01:36:59 +0200

    Improve the tarball script so that it creates the directory and accepts a version provided.

--- tarball.sh
+++ tarball.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-git-ls-files | xargs tar -jcf gitarella-$(date +"%Y-%m-%d_%H-%M").tar.bz2
+if [[ $# -ge 1 ]]; then
+  version=$1
+else
+  version=$(date +"%Y-%m-%d_%H-%M")
+fi
+
+rm -rf "gitarella-${version}"
+mkdir "gitarella-${version}"
+git-ls-files | xargs tar cf - | tar xf - -C "gitarella-${version}"
+
+tar -jcf "gitarella-${version}.tar.bz2" "gitarella-${version}"
