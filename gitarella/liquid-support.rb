932b225fd9086c7567efe9c9c87189b894d9aca9
Diego 'Flameeyes' Pettenò <flameeyes@gentoo.org>
2006-07-02 03:53:25 +0200

    Implement an AlignedFor liquid block, so taht the lines count on the blob view is actually aligned correctly.

--- gitarella/liquid-support.rb
+++ gitarella/liquid-support.rb
@@ -47,7 +47,65 @@ module Gitarella
       end
    end
 
+   class AlignedFor < Liquid::Block
+      Syntax = /(\w+)\s+in\s+(#{Liquid::AllowedVariableCharacters}+)/
+
+      def initialize(markup, tokens)
+         super
+
+         if markup =~ Syntax
+            @variable_name = $1
+            @collection_name = $2
+         else
+            raise SyntaxError.new("Syntax Error in 'afor loop' - Valid syntax: afor [item] in [collection]")
+         end
+      end
+
+      def render(context)
+         collection = context[@collection_name] or raise ArgumentError.new("Error in 'for loop' - '#{@collection_name}' does not exist.")
+         length = collection.length
+
+         result = []
+
+         context.stack do
+            collection.each_with_index do |item, index|
+               length_digits = length.to_s.size
+               idx_digits = (index+1).to_s.size
+               ridx_digits = (length - index).to_s.size
+
+               alignment = ""
+               while idx_digits < length_digits
+                  alignment = alignment + " "
+                  idx_digits += 1
+               end
+
+               ralignment = ""
+               while ridx_digits < length_digits
+                  ralignment = ralignment + " "
+                  ridx_digits += 1
+               end
+
+               context[@variable_name] = item
+               context['forloop'] = {
+                  'length'  => length,
+                  'aligidx' => alignment,
+                  'index'   => index + 1,
+                  'index0'  => index,
+                  'rindex'  => length - index,
+                  'rindex0' => length - index -1,
+                  'raligidx'=> ralignment,
+                  'first'   => (index == 0),
+                  'last'    => (index == length - 1) }
+
+               result << render_all(@nodelist, context)
+            end
+         end
+         result
+      end
+   end
+
    Liquid::Template.register_filter(LiquidFilters)
+   Liquid::Template.register_block('alignedfor', Gitarella::AlignedFor)
 end
 
 # kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 3;
