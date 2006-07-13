b057e955a5326731ed10e766d93522e3ac09488b
Diego 'Flameeyes' Pettenò <flameeyes@gentoo.org>
2006-07-13 21:33:42 +0200

    Fix indentiation.

--- gitarella/globals.rb
+++ gitarella/globals.rb
@@ -16,47 +16,46 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 module Gitarella
-   class Globals
-      @@cache = Hash.new
-      def Globals.cache
-         @@cache
-      end
+class Globals
+   @@cache = Hash.new
+   def Globals.cache
+      @@cache
+   end
 
-      def Globals.init_cache
-         return unless $config["memcache-servers"] and not $config["memcache-servers"].empty?
+   def Globals.init_cache
+      return unless $config["memcache-servers"] and not $config["memcache-servers"].empty?
 
-         begin
-            require "memcache"
-            memcache = MemCache::new($config["memcache-servers"], :namespace => 'gitarella', :compression => 'true')
-            memcache["gitarella-test"] = true
-            @@cache = memcache
-         rescue LoadError
-            $log.error "memcache configured, but unable to load 'memcache' extension."
-         rescue MemCache::MemCacheError
-            $log.error "memcache configured, but no server available."
-         end
-      end
-
-      @@repos = Hash.new
-      def Globals.repos
-         @@repos
-      end
-
-      def Globals.init_repos
-         @@repos = Hash.new
-
-         $config["repositories"].each { |repo|
-            gitrepo = GITRepo.new(repo)
-            @@repos[gitrepo.id] = gitrepo
-         }
-      end
-
-      def Globals.init_all
-         init_cache
-         init_repos
+      begin
+         require "memcache"
+         memcache = MemCache::new($config["memcache-servers"], :namespace => 'gitarella', :compression => 'true')
+         memcache["gitarella-test"] = true
+         @@cache = memcache
+      rescue LoadError
+         $log.error "memcache configured, but unable to load 'memcache' extension."
+      rescue MemCache::MemCacheError
+         $log.error "memcache configured, but no server available."
       end
    end
 
+   @@repos = Hash.new
+   def Globals.repos
+      @@repos
+   end
+
+   def Globals.init_repos
+      @@repos = Hash.new
+
+      $config["repositories"].each { |repo|
+         gitrepo = GITRepo.new(repo)
+         @@repos[gitrepo.id] = gitrepo
+      }
+   end
+
+   def Globals.init_all
+      init_cache
+      init_repos
+   end
+end
 end
 
 # kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 3;
